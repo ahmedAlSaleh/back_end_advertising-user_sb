a68b9a5004c17fcd01dc23ec8ee83f92c798f152
AHMEDASML <asmlahmd9@gmail.com>
2026-04-27 12:21:46 +0300

    Backend import batch 28

---
+++ app/Filament/Admin/Resources/UserResource/Pages/CreateUser.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Filament\Admin\Resources\UserResource\Pages;
+
+use App\Filament\Admin\Resources\UserResource;
+use Filament\Actions;
+use Filament\Notifications\Notification;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateUser extends CreateRecord
+{
+    protected static string $resource = UserResource::class;
+
+    protected function getCreatedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->success()
+            ->title('User created')
+            ->body('The user has been created successfully.');
+    }
+}
