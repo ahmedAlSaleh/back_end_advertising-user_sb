9a5a6aab3a44a87c75f14205e1645de9a335e28e
AHMEDASML <asmlahmd9@gmail.com>
2026-04-27 12:22:36 +0300

    Backend import batch 38

---
+++ lang/ar/navigation.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'dashboard' => 'لوحة التحكم',
+
+    // Navigation Groups
+    'Users Management' => 'إدارة المستخدمين',
+    'Store Management' => 'إدارة المتاجر',
+    'Content Management' => 'إدارة المحتوى',
+    'Engagement' => 'التفاعل',
+    'Financial' => 'المالية',
+    'System' => 'النظام',
+
+    // Resources
+    'users' => 'المستخدمون',
+    'traders' => 'التجار',
+    'stores' => 'المتاجر',
+    'categories' => 'الفئات',
+    'advertisements' => 'الإعلانات',
+    'posts' => 'المنشورات',
+    'wallets' => 'المحافظ',
+    'recharge_codes' => 'أكواد الشحن',
+    'recharge_operations' => 'عمليات الشحن',
+    'reviews' => 'التقييمات',
+];
