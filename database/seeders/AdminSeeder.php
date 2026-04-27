84686cd841f2d46341f24a61cbca2b42f185093d
AHMEDASML <asmlahmd9@gmail.com>
2026-04-27 12:21:41 +0300

    Backend import batch 27

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Admin;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Admin::create([
+            'name' => 'Super Admin',
+            'phone' => '1234567890',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password'),
+        ]);
+    }
+}
